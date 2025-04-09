1c879a88f58a11d54ad32dad20ce182c057d4e2e
dag.bjornberg <dag.bjornberg@softwerk.se>
2025-04-09 14:09:41 +0200

    added no transfer

--- boosting/config.py
+++ boosting/config.py
@@ -23,3 +23,6 @@ test_seed_list = [1]
 train_seed_list = [1]
 
 ## non-transfer config
+vs =[0.003, 0.005, 0.007, 0.01, 0.03, 0.05, 0.07, 0.1]
+tree_size_list = [1,2,3,4, 5, 6]
+epoch_list_no_transfer = [200, 400, 600, 800]
